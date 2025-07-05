ba9dc9a5d65e12a7f8580e8feb6f9492d4db0121
Prakash Sahu <pk484442@gmail.com>
2025-07-05 17:26:36 +0530

    Add basic clipboard data structures
    - Define ClipboardItem struct
    - Add clipboard history to App struct
    - Implement GetClipboardHistory method

--- app.go
+++ app.go
@@ -2,17 +2,26 @@ package main
 
 import (
 	"context"
-	"fmt"
+	"time"
 )
+
+type ClipboardItem struct {
+	ID        string    `json:"id"`
+	Content   string    `json:"content"`
+	Timestamp time.Time `json:"timestamp"`
+}
 
 // App struct
 type App struct {
-	ctx context.Context
+	ctx              context.Context
+	clipboardHistory []ClipboardItem
 }
 
 // NewApp creates a new App application struct
 func NewApp() *App {
-	return &App{}
+	return &App{
+		clipboardHistory: make([]ClipboardItem, 0),
+	}
 }
 
 // startup is called when the app starts. The context is saved
@@ -21,7 +30,7 @@ func (a *App) startup(ctx context.Context) {
 	a.ctx = ctx
 }
 
-// Greet returns a greeting for the given name
-func (a *App) Greet(name string) string {
-	return fmt.Sprintf("Hello %s, It's show time!", name)
+// GetClipboardHistory returns the clipboard history
+func (a *App) GetClipboardHistory() []ClipboardItem {
+	return a.clipboardHistory
 }
